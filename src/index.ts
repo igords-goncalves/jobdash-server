bfc399298a0970b5a3c64c665444ee1d0ee37543
Igor Gonçalves <igordossantos38@gmail.com>
2025-04-15 17:46:28 -0300

    feat: add sample environment configuration and update TODO for server setup

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,33 @@
-import { fastify, FastifyReply, FastifyRequest} from "fastify";
-import db from "@fastify/postgres"
+import { fastify, FastifyReply, FastifyRequest } from "fastify";
+import db from "@fastify/postgres";
 
-const app = fastify({
-  logger: true,
-});
+const app = fastify();
 
 app.register(db, {
   connectionString: process.env.DATABASE_URL,
-})
+});
 
 app.get("/ping", (request: FastifyRequest, reply: FastifyReply) => {
-  reply.send("pong pong pong!");
+  reply.send("pong!");
 });
 
-app.get('/users', async (request: FastifyRequest, reply: FastifyReply) => {
-  const response = await app.pg.query(
-    'SELECT * FROM users',
-  )
+app.get("/users", async (request: FastifyRequest, reply: FastifyReply) => {
+  const response = await app.pg.query("SELECT * FROM users");
+  return reply.send(response.rows);
+});
 
-  await reply.send(response.rows);
-})
+app.get("/jobs", async (request: FastifyRequest, reply: FastifyReply) => {
+  const response = await app.pg.query("SELECT * FROM applications");
+  return reply.send(response.rows);
+});
 
-app.listen({ port: 3333 , host: "0.0.0.0"}, (err, address) => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
+app.listen(
+  { port: Number(process.env.PORT), host: "0.0.0.0" },
+  (err, address) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
   }
-  console.log(`Server listening at ${address}`);
-  app.log.info(`Server listening at ${address}`);
-});
+);
